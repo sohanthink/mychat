88a30990294b888c516a19577dcf7e555c529131
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-01-06 22:26:29 +0600

    project setup for chatting application.

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+
+function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Login />} />
+        <Route path="/registration" element={<Register />} />
+      </>
+    )
+  );
+
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
