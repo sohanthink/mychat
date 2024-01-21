40e29df1a09976840d8aa191bd8b37f0c6a78194
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-01-21 23:19:13 +0600

    register page validation complted with Formik and yup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,29 +13,29 @@ import BlockedUser from './BlockedUser'
 const Home = () => {
     return (
         <Box sx={{ flexGrow: 1, padding: '25px' }}>
-            <Grid container spacing={0}>
+            <Grid container spacing={0.5}>
 
-                <Grid xs={4} sx={{ marginBottom: '30px' }}>
+                <Grid xs={12} xl={4} sx={{ marginBottom: '30px' }}>
                     <GroupList />
                 </Grid>
 
-                <Grid xs={4} sx={{ padding: '0px 30px', marginBottom: '30px' }}>
+                <Grid xs={12} xl={4} sx={{ padding: '0px 30px', marginBottom: '30px' }}>
                     <Friends />
                 </Grid>
 
-                <Grid xs={4} sx={{ marginBottom: '30px' }}>
+                <Grid xs={12} xl={4} sx={{ marginBottom: '30px' }}>
                     <UserList />
                 </Grid>
 
-                <Grid xs={4} sx={{ marginBottom: '30px' }}>
+                <Grid xs={12} xl={4} sx={{ marginBottom: '30px' }}>
                     <FriendRequest />
                 </Grid>
 
-                <Grid xs={4} sx={{ padding: '0px 30px', marginBottom: '30px' }}>
+                <Grid xs={12} xl={4} sx={{ padding: '0px 30px', marginBottom: '30px' }}>
                     <MyGroups />
                 </Grid>
 
-                <Grid xs={4} sx={{ marginBottom: '30px' }}>
+                <Grid xs={12} xl={4} sx={{ marginBottom: '30px' }}>
                     <BlockedUser />
                 </Grid>
 
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -4,14 +4,15 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import SectionHeading from '../../components/SectionHeading';
-import LoginImg from '../../../public/login/login.png';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import Image from '../../utilities/Image/Image';
 import './register.css'
 import Paragraph from '../../utilities/Paragraph';
 import RegImg from '../../assets/images/registration/registration.png';
-
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Alert } from '@mui/material';
 
 
 const BootstrapButton = styled(Button)({
@@ -27,7 +28,35 @@ const BootstrapButton = styled(Button)({
 });
 
 
-const Login = () => {
+const Register = () => {
+
+    // Validation code Using Formik ==============================================================
+
+    let emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            password: '',
+        },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .max(25, 'Must be 25 characters or less')
+                .required('Name Required'),
+            password: Yup.string()
+                .max(15, 'Must be 15 characters or less')
+                .required(' Password Required'),
+            email: Yup.string()
+                .matches(emailRegex, 'Invalid email address')
+                .email('Invalid email address')
+                .required('Email Required'),
+        }),
+        onSubmit: values => {
+            // alert(JSON.stringify(values, null, 2));
+            console.log(values);
+        },
+    });
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={0}>
@@ -41,17 +70,58 @@ const Login = () => {
                         <Box>
                             <SectionHeading style='section_heading' text="Get started with easily register" />
                             <Paragraph style="reg_subheading" text='Free register and you can enjoy it.' />
-                            <div className="login_input">
-                                <TextField fullWidth id="email" label="Email Addres" variant="outlined" />
-                                <TextField fullWidth id="name" label="Ful name" variant="outlined" />
-                                <TextField fullWidth id="password" label="Password" type="password" variant="outlined" />
-                            </div>
-                            <div className="btn">
-                                {/* <Button fullWidth variant="contained">Login to Continue</Button> */}
-                                <BootstrapButton fullWidth variant="contained">
-                                    Sign up
-                                </BootstrapButton>
-                            </div>
+                            <form onSubmit={formik.handleSubmit}>
+                                <div className="login_input">
+                                    <div>
+                                        <TextField onChange={formik.handleChange}
+                                            value={formik.values.email}
+                                            fullWidth name='email'
+                                            id="email"
+                                            label="Email Addres"
+                                            variant="outlined" />
+
+                                        {
+                                            formik.touched.email && formik.errors.email ? (
+                                                <Alert severity="error">{formik.errors.email}</Alert>
+                                            ) : null
+                                        }
+                                    </div>
+                                    <div>
+                                        <TextField onChange={formik.handleChange}
+                                            value={formik.values.name}
+                                            fullWidth name='name'
+                                            id="name"
+                                            label="Ful name"
+                                            variant="outlined" />
+
+                                        {
+                                            formik.touched.name && formik.errors.name ? (
+                                                <Alert severity="error">{formik.errors.name}</Alert>
+                                            ) : null
+                                        }
+                                    </div>
+                                    <div>
+                                        <TextField onChange={formik.handleChange}
+                                            value={formik.values.password}
+                                            fullWidth name='password'
+                                            id="password"
+                                            label="Password"
+                                            type="password"
+                                            variant="outlined" />
+
+                                        {
+                                            formik.touched.password && formik.errors.password ? (
+                                                <Alert severity="error">{formik.errors.password}</Alert>
+                                            ) : null
+                                        }
+                                    </div>
+                                </div>
+                                <div className="btn">
+                                    <BootstrapButton type='submit' fullWidth variant="contained">
+                                        Sign up
+                                    </BootstrapButton>
+                                </div>
+                            </form>
                             <div className="login_footer">
                                 <h5>Already  have an account ?  <Link to="/"> <span> Sign In</span> </Link></h5>
                             </div>
@@ -64,4 +134,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default Register
