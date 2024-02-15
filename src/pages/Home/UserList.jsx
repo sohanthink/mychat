24c00c2eb0702289a3980215fabac7af07b60faf
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-02-16 00:03:11 +0600

    uselist add on the homepage from firebase rt db

--- src/pages/Home/UserList.jsx
+++ src/pages/Home/UserList.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaUserPlus } from "react-icons/fa";
 import GroupCard from '../../components/home/GroupCard'
 import Image from '../../utilities/Image/Image'
-
+import { getDatabase, ref, onValue } from "firebase/database";
 
 const UserList = () => {
+    let [userList, setUserList] = useState([])
+
+    const db = getDatabase();
+
+
+    // userlist from datatbase
+    useEffect(() => {
+        const usersRef = ref(db, 'users/');
+        onValue(usersRef, (snapshot) => {
+            // const data = snapshot.val();
+            let arr = []
+            snapshot.forEach(item => {
+                arr.push(item.val())
+            })
+            setUserList(arr)
+        });
+        // console.log(userList);
+    }, [])
+
+
+
     return (
         <GroupCard cardtitle='User Lists'>
             {
-                [0, 1, 2, 3, 4, 5, 6].map((item, index) => (
+                userList.map((item, index) => (
                     <div key={index} className="usermainbox">
                         <div className="useritem">
                             <div className="userimagebox">
-                                <Image source="" alt='Image' />
+                                <Image source={item.profileImg} alt='Image' />
                             </div>
                             <div className="userinfobox">
                                 <div>
-                                    <h3>Tejeshwini C</h3>
-                                    <p>Yesterday, 6:22pm</p>
+                                    <h3>{item.name}</h3>
+                                    <p>{item.email}</p>
                                 </div>
                                 <button className='userbutton'>
                                     <FaUserPlus />
