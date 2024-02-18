b3c4a1074de1811da2b6174b6d22a3dddc9a5c22
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-02-18 15:49:10 +0600

    friend request send

--- src/pages/Home/UserList.jsx
+++ src/pages/Home/UserList.jsx
@@ -2,21 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { FaUserPlus } from "react-icons/fa";
 import GroupCard from '../../components/home/GroupCard'
 import Image from '../../utilities/Image/Image'
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
 import { useSelector } from 'react-redux'
 
 
 
 const UserList = () => {
     let [userList, setUserList] = useState([])
+    let [friendRequest, SetFriendRequest] = useState([])
 
 
     const db = getDatabase();
 
     const data = useSelector(state => state.loginuserdata.value)
+    console.log(data);
 
-
-    // userlist from datatbase
+    // userlist from datatbase ===========================================
     useEffect(() => {
         const usersRef = ref(db, 'users/');
         onValue(usersRef, (snapshot) => {
@@ -24,13 +25,42 @@ const UserList = () => {
             let arr = []
             snapshot.forEach(item => {
                 if (data.uid != item.key) {
-                    arr.push(item.val())
+                    arr.push({ ...item.val(), id: item.key })
                 }
             })
             setUserList(arr)
         });
         // console.log(userList);
     }, [])
+
+
+    // send friendrequest data write to  datatbase =========================
+    let handleFriendRequest = (item) => {
+        // console.log("k click korche :", data.uid);
+        // console.log("kake click korche :", item.id);
+        set(push(ref(db, 'friendrequest/')), {
+            whosendid: data.uid,
+            whosendname: data.displayName,
+            whoreceivedid: item.id,
+            whoreceivedname: item.name,
+        });
+    }
+
+    // friendrequest read from datatbase ===========================================
+    useEffect(() => {
+        const friendrequestRef = ref(db, 'friendrequest/');
+        onValue(friendrequestRef, (snapshot) => {
+            // const data = snapshot.val();
+            let arr = []
+            snapshot.forEach(item => {
+                arr.push(item.val().whoreceivedid + item.val().whosendid)
+            })
+            SetFriendRequest(arr)
+        });
+        // console.log(friendRequest);
+    }, [])
+
+
 
 
 
@@ -48,9 +78,20 @@ const UserList = () => {
                                     <h3>{item.name}</h3>
                                     <p>{item.email}</p>
                                 </div>
-                                <button className='userbutton'>
-                                    <FaUserPlus />
-                                </button>
+                                {
+                                    friendRequest.includes(item.id + data.uid)
+                                        ?
+                                        <button onClick={() => handleFriendRequest(item)} className='userbutton'>
+                                            cancel
+                                        </button>
+                                        :
+                                        <button onClick={() => handleFriendRequest(item)} className='userbutton'>
+                                            <FaUserPlus />
+                                        </button>
+
+                                }
+
+
                             </div>
                         </div>
                     </div>
