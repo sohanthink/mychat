796023fe7bdebca11167cd06c1a2f1182e44eed2
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-02-24 02:36:56 +0600

    design update

--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -1,7 +1,14 @@
 import { Box, Grid } from '@mui/material'
 import React from 'react'
+import Posts from './Posts'
+import './feed.css'
+
+
+
+
 
 const Feed = () => {
+
     return (
         <>
             <Box sx={{ flexGrow: 1, padding: '10px' }}>
@@ -9,6 +16,10 @@ const Feed = () => {
 
                     <Grid xs={12} md={12} xl={12} sx={{ marginBottom: '30px' }}>
                         <h2>things will go here</h2>
+                    </Grid>
+
+                    <Grid xs={12} md={12} xl={12} sx={{ marginBottom: '30px' }}>
+                        <Posts />
                     </Grid>
 
                 </Grid>
